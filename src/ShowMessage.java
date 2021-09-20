05e4d788472efe57826ad5db03e1b9dd539f5b7f
gsolorzano <giovana.solorzano@alten.it>
2021-09-20 12:49:11 +0200

    Add functionality - showMessage

---
+++ src/ShowMessage.java
@@ -0,0 +1,8 @@
+public class ShowMessage {
+    public void showMsg(){
+        System.out.println("Hi everyone!");
+    }
+    public void showMsg(String name){
+        System.out.println("Hello " + name);
+    }
+}
